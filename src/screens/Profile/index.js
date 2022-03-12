a59c1441242e1b2309ff87f757b089c35538fa12
Marcos Victor <marcosvictorsb@gmail.com>
2022-03-12 17:52:34 -0300

    criar a funcinalidade de logout

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Button } from 'react-native';
+import Api from '../../Api';
 import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
 
 export default () => {
-  return (
-    <Container>
-      <Text>Profile</Text>
-    </Container>
-  )
+    const navigation = useNavigation();
+
+    const handleLogoutClick = async () => {
+        await Api.logout();
+        navigation.reset({
+            routes: [{ name: 'SignIn' }]
+        })
+    }
+
+
+    return (
+        <Container>
+            <Text>Profile</Text>
+            <Button title="Sair" onPress={handleLogoutClick}/>
+        </Container>
+    )
 }
